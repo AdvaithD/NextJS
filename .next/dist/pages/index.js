6525b51cd29cf09d60081b3f7c7b5a655669f44c
Advaith Doosa <advaithseo@gmail.com>
2017-10-30 21:31:35 -0700

    client side routing implemented

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/advaith/code/learning/nextjs/pages/index.js?entry";
+// This is the Link API
+
+
+var Index = function Index() {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement(_link2.default, { href: "/about", style: { fontSize: 20 }, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, _react2.default.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, "About Page")), _react2.default.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    }
+  }, "Hello Next.js"));
+};
+
+exports.default = Index;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxpbmsiLCJJbmRleCIsImZvbnRTaXplIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQ0EsQUFBTzs7Ozs7OztBQURQOzs7QUFHQSxJQUFNLFFBQVEsU0FBUixBQUFRLFFBQUE7eUJBQ1osY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRSxBQUFDLGdDQUFLLE1BQU4sQUFBVyxVQUFTLE9BQU8sRUFBRSxVQUE3QixBQUEyQixBQUFZO2dCQUF2QztrQkFBQSxBQUNFO0FBREY7cUJBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBRkosQUFDRSxBQUNFLEFBRUYsZ0NBQUEsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBTFUsQUFDWixBQUlFO0FBTEosQUFTQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMvYWR2YWl0aC9jb2RlL2xlYXJuaW5nL25leHRqcyJ9
